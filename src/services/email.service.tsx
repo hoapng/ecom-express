867b5b4101c14386fc8451f392959d419a74b5ba
hoapng <giahoa01giathieu@gmail.com>
2025-07-14 15:07:40 +0700

    Sử dụng React Email làm Email Template

--- src/services/email.service.tsx
+++ src/services/email.service.tsx
@@ -1,15 +1,18 @@
 import { Resend } from 'resend'
 import envConfig from '~/config/evnConfig'
+import * as React from 'react'
+import { OTPEmail } from '~/view/otp'
 
 export class EmailService {
   private static resend = new Resend(envConfig.RESEND_API_KEY)
 
   static sendOTP(payload: { email: string; code: string }) {
+    const subject = 'Mã OTP'
     return this.resend.emails.send({
       from: 'Ecommerce <onboarding@resend.dev>',
       to: ['giahoa01giathieu@gmail.com'],
-      subject: 'Mã OTP',
-      html: `<strong>${payload.code}</strong>`
+      subject,
+      react: <OTPEmail otpCode={payload.code} title={subject} />
     })
   }
 }
