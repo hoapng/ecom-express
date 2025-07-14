2e697f870bb336e28b6707ed8867351b6b833b68
hoapng <giahoa01giathieu@gmail.com>
2025-07-14 10:08:08 +0700

    setup global type

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,12 +1,13 @@
 import { StatusCodes } from 'http-status-codes'
 import { TokenPayload } from './types/jwt.type'
+import { REQUEST_USER_KEY } from './constants/auth.constant'
 
 declare global {
   namespace Express {
     interface Request {
       data?: any
       statusCode?: StatusCodes
-      user?: TokenPayload
+      [REQUEST_USER_KEY]?: TokenPayload
       [key: string]: any // Allow additional properties
     }
   }
