f975647e2b92dee18c13151422865aa3d110b1c1
hoapng <giahoa01giathieu@gmail.com>
2025-07-23 17:23:52 +0700

    File Validation

--- src/routes/media.route.ts
+++ src/routes/media.route.ts
@@ -4,10 +4,16 @@ import { mediaController } from '~/controllers/media.controller'
 import { wrapRequestHandler } from '~/utils/wrapRequestHandler'
 import path from 'path'
 import { generateRandomFilename } from '~/utils/helper'
+import { existsSync, mkdirSync } from 'fs'
 
 const mediaRouter = Router()
 
 const UPLOAD_DIR = path.resolve('upload')
+
+if (!existsSync(UPLOAD_DIR)) {
+  mkdirSync(UPLOAD_DIR, { recursive: true })
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, UPLOAD_DIR)
@@ -18,7 +24,16 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage })
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1 * 1024 * 1024 // 1MB
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedImageTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
+    cb(null, allowedImageTypes.includes(file.mimetype))
+  }
+})
 
 mediaRouter.post(
   '/images/upload',
